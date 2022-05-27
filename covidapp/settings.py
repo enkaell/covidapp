ecfd75a3f1dbecbd1002c2fd8462e6c5396ed03b
vlad <vlad2012chesnokov@mail.ru>
2022-05-27 16:44:09 +0500

    remove cors

--- covidapp/settings.py
+++ covidapp/settings.py
@@ -24,7 +24,9 @@ SECRET_KEY = 'django-insecure-692qumslwvg)tmt-pq)g(he@vj_lq8sbx@fnue&x1n1^*^gm&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'covid',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'covidapp.urls'
